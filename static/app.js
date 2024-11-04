128102963c5c8c1db5e810aa4565a961a2a5e15e
Fernando Valencia <91638925+Sembofer@users.noreply.github.com>
2024-11-04 11:02:04 -0600

    Update app.js

--- static/app.js
+++ static/app.js
@@ -45,8 +45,9 @@ class Chatbox {
 
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
+        const API_URL = "https://https://chatbot-py-js.onrender.com";
 
-        fetch('http://127.0.0.1:5000/predict', {
+        fetch('${API_URL}/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
