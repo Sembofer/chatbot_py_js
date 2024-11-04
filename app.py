8eb9a6fe1273cb49dead62c47298ef3bc0278c57
Fernando Valencia <91638925+Sembofer@users.noreply.github.com>
2024-11-04 10:04:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 CORS(app)
 intents = json.loads(open('intents.json', 'r', encoding='utf-8').read())
 
-@app.get("/")
+#@app.get("/")
 def index_get():
     return render_template("base.html")
 
 
 
-@app.post("/predict")
+@app.route("/predict")
+@cross_origin(origins=["https://chatbot-py-js.onrender.com"])
 def predict():
     text = request.get_json().get("message")
     # TODO:check if text is valid
