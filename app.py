9d7df41a8a0c11559b371b550422d9b4cbe9764c
Fernando Valencia <91638925+Sembofer@users.noreply.github.com>
2024-11-04 15:32:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chatbot import get_response, predict_class
 import json
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins":"*", "methods":["POST"]}})
@@ -25,4 +26,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
