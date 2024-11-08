183167e2de76589b57bd0cbd29f26b828888b550
Fernando Valencia <91638925+Sembofer@users.noreply.github.com>
2024-11-08 11:53:23 -0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -18,8 +18,8 @@ classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.keras')
 
 def clean_up_sentence(sentence):
-    #nltk.download('punkt_tab')
-    #nltk.download('wordnet')
+    nltk.download('punkt_tab')
+    nltk.download('wordnet')
     #sleep(200)
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
